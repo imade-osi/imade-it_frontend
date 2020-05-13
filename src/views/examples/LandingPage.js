594ea4706d910e932e4a6a7ecacf1251fe495de9
Imade Osifo <toxsik88@gmail.com>
2020-05-12 22:35:01 -0400

    added additional debuggers for edit

--- src/views/examples/LandingPage.js
+++ src/views/examples/LandingPage.js
@@ -146,6 +146,7 @@ export default class LandingPage extends Component {
                 }) 
               }
               //debugger 
+              //debugger
             this.setCenter(latCenter, longCenter, markersArray)
           })
       })
