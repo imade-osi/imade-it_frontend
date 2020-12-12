89a9a8043974d43087d17822d49108b4daf2e153
Imade Osifo <toxsik88@gmail.com>
2020-12-12 14:24:50 -0500

    updated state for map page

--- src/views/examples/LandingPage.js
+++ src/views/examples/LandingPage.js
@@ -65,7 +65,7 @@ export default class LandingPage extends Component {
     if (!city)
     {
     city = 'los angeles'
-    state = 'pennsylvania'
+    state = 'california'
     }
     // city = 'chicago'
     // state = 'illinois'
